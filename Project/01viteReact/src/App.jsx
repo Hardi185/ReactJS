758ecf6c4184121a0c40b4ae60c080b72cb2d3ce
hardiJadvani <hardirj185@gmail.com>
2024-11-14 21:12:48 +0530

    Added different element formats to test by renderer

--- Project/01viteReact/src/App.jsx
+++ Project/01viteReact/src/App.jsx
@@ -1,9 +1,11 @@
 import Chai from './chai'
 
 function App() {
+  const name = "Hello Hardi"
   return (
     <>
       <Chai/>
+      <h1>{name}</h1>
     </>
   )
 }
--- Project/01viteReact/src/main.jsx
+++ Project/01viteReact/src/main.jsx
@@ -1,9 +1,34 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
+import ReactDOM from 'react-dom'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const testVar = "Hello Hardi"
+
+//check which element of react is supported by yourself
+const reactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'Click me to visit google'
+}
+
+const reactElement2 = React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'Click me to visit google',
+  testVar
+)
+
+const reactElement3 = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    // <App />
+    //reactElement
+    // reactElement2
+    reactElement3
 )
