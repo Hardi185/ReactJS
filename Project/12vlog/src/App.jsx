fb865468044126fcbc29024d290e857b0ec42931
hardiJadvani <hardirj185@gmail.com>
2024-11-17 01:00:35 +0530

    Added Blog prg

---
+++ Project/12vlog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL)
+  return (
+    <>
+      <div className='bg-green-500'>This is vlog</div>
+    </>
+  )
+}
+
+export default App
