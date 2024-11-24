a549f79a660fe82916ab584e33bfef181cbb9701
hardiJadvani <hardirj185@gmail.com>
2024-11-24 14:22:39 +0530

    Added Input and Button component

--- Project/12vlog/src/components/index.js
+++ Project/12vlog/src/components/index.js
@@ -1,9 +1,13 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-// import Container from "./Container/Container"
+import Container from "./Container/Container"
+import LogoutBtn from "./Header/LogoutBtn"
+import Logo from "./Logo"
 
 export{
     Header, 
     Footer,
-    // Container
+    Container,
+    LogoutBtn,
+    Logo
 }
