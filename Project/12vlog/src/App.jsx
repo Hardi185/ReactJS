68231ffb8b07097a7d3d535b6c2e7b39dba4fd32
hardiJadvani <hardirj185@gmail.com>
2024-11-24 12:59:16 +0530

    Added basic components

--- Project/12vlog/src/App.jsx
+++ Project/12vlog/src/App.jsx
@@ -1,15 +1,43 @@
+import React, { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import './App.css'
-import conf from './conf/config'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  console.log(conf.appwriteBucketId)
-  return (
-    <>
-      <div className='bg-green-500'>This is vlog</div>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(login({userDatas}))
+      }
+      //called else to handle else
+      //so that atleast the state will be updated to false
+      else{
+        dispatch(logout())
+      }
+    })
+    //after all this loading needed to be stop
+    .finally(() => setLoading(false))
+  })
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO:  <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
