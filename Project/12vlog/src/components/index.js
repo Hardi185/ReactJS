e103091bcee729d86e7b0ee266718a436fa56f94
hardiJadvani <hardirj185@gmail.com>
2024-11-24 20:18:53 +0530

    Added Login and Signup jsx

--- Project/12vlog/src/components/index.js
+++ Project/12vlog/src/components/index.js
@@ -3,11 +3,21 @@ import Footer from "./Footer/Footer";
 import Container from "./Container/Container"
 import LogoutBtn from "./Header/LogoutBtn"
 import Logo from "./Logo"
+import Login from "./Login"
+import Button from "./Header/Button"
+import Select from "./Select"
+import Input from "./Input";
+import PostCard from "./PostCard"
 
 export{
     Header, 
     Footer,
     Container,
     LogoutBtn,
-    Logo
+    Logo,
+    Login,
+    Button,
+    Select,
+    Input,
+    PostCard
 }
